f767d3d5757e5bf5bbfea52196b38b1ebfa88951
cauacouto <cauacouto022@gmail.com>
2026-06-09 22:41:28 -0300

    initialize microservice project with Spring Boot and Maven setup

---
+++ src/main/java/dev/couto/microsservice_reserva/MicrosserviceReservaApplication.java
@@ -0,0 +1,13 @@
+package dev.couto.microsservice_reserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosserviceReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosserviceReservaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/couto/microsservice_reserva/MicrosserviceReservaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.couto.microsservice_reserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosserviceReservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
