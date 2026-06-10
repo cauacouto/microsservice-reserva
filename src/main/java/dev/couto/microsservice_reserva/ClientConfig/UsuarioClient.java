a7c446ca00eaebb0b57f4c7a32d45ca8aa7fcc50
cauacouto <cauacouto022@gmail.com>
2026-06-09 22:42:00 -0300

    add UsuarioClient to handle communication with the Usuario service

---
+++ src/main/java/dev/couto/microsservice_reserva/ClientConfig/UsuarioClient.java
@@ -0,0 +1,38 @@
+package dev.couto.microsservice_reserva.ClientConfig;
+
+import dev.couto.microsservice_reserva.Dto.UsuarioDtoResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.UUID;
+
+@Service
+
+public class UsuarioClient {
+
+
+    private final WebClient webClient;
+
+
+     public UsuarioClient(
+             WebClient.Builder builder,
+             @Value("${services.usuario.url}") String baseUrl){
+
+         this.webClient = builder
+                 .baseUrl(baseUrl)
+                 .build();
+     }
+
+     public UsuarioDtoResponse buscarUsuario(UUID id){
+         return webClient.get()
+                 .uri("/usuarios/{id}",id)
+                 .retrieve()
+                 .bodyToMono(UsuarioDtoResponse.class)
+                 .block();
+     }
+
+
+
+
+}
