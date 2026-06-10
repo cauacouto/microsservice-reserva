7ceb367a884aa370406d714c1ee5249e1f9465f1
cauacouto <cauacouto022@gmail.com>
2026-06-09 22:41:35 -0300

    add Reserva entity with fields for reservation details

---
+++ src/main/java/dev/couto/microsservice_reserva/domin/Reserva.java
@@ -0,0 +1,38 @@
+package dev.couto.microsservice_reserva.domin;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import dev.couto.microsservice_reserva.Enum.statusReserva;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "reserva")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Reserva implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Integer id;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime dataInicio;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime dataFim;
+    @Enumerated(value = EnumType.STRING)
+    private statusReserva status;
+    private UUID usuarioId;
+    private Integer salaId;
+
+
+
+
+}
