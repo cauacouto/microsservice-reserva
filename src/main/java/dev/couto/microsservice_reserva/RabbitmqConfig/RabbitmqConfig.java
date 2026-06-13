eccf2bc779f288938666e39f3d5cf02727119b2c
cauacouto <cauacouto022@gmail.com>
2026-06-13 00:33:44 -0300

    add RabbitmqConfig class for RabbitMQ configuration and message handling

---
+++ src/main/java/dev/couto/microsservice_reserva/RabbitmqConfig/RabbitmqConfig.java
@@ -0,0 +1,45 @@
+package dev.couto.microsservice_reserva.RabbitmqConfig;
+
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter(){
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,JacksonJsonMessageConverter converter){
+
+        RabbitTemplate template =
+                new RabbitTemplate(connectionFactory);
+
+        template.setMessageConverter(converter);
+        return template;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicilizarAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public FanoutExchange reservaExchange(){
+        return new FanoutExchange("reserva.ex");
+    }
+}
