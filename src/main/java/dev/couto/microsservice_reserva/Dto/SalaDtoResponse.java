533994d75ff73fb934dbf44a70b393969c1689dc
cauacouto <cauacouto022@gmail.com>
2026-06-13 00:35:18 -0300

    add status field to SalaDtoResponse for room status representation

--- src/main/java/dev/couto/microsservice_reserva/Dto/SalaDtoResponse.java
+++ src/main/java/dev/couto/microsservice_reserva/Dto/SalaDtoResponse.java
@@ -1,7 +1,10 @@
 package dev.couto.microsservice_reserva.Dto;
 
+import dev.couto.microsservice_reserva.Enum.StatusSala;
+
 public record SalaDtoResponse(
         Integer id,
-        boolean ativo
+        StatusSala status
+
 ) {
 }
