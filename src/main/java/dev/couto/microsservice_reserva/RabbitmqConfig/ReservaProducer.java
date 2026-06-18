cc31af98076463f32c3d0a64912391c3686d16c8
cauacouto <cauacouto022@gmail.com>
2026-06-17 22:35:35 -0300

    refactor: rename RecervaProducer to ReservaProducer for consistency

--- src/main/java/dev/couto/microsservice_reserva/RabbitmqConfig/ReservaProducer.java
+++ src/main/java/dev/couto/microsservice_reserva/RabbitmqConfig/ReservaProducer.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class RecervaProducer {
+public class ReservaProducer {
 
 
     private final RabbitTemplate rabbitTemplate;
