765f412119518932a1153c457314a033e6dd1a56
cauacouto <cauacouto022@gmail.com>
2026-06-17 22:15:32 -0300

    add: implement RecervaProducer for sending ReservaProducerDto to RabbitMQ

---
+++ src/main/java/dev/couto/microsservice_reserva/RabbitmqConfig/RecervaProducer.java
@@ -0,0 +1,25 @@
+package dev.couto.microsservice_reserva.RabbitmqConfig;
+
+import dev.couto.microsservice_reserva.Dto.ReservaProducerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RecervaProducer {
+
+
+    private final RabbitTemplate rabbitTemplate;
+    private final FanoutExchange reservaExchange;
+
+
+    public void enviar(ReservaProducerDto reservaProducer){
+        rabbitTemplate.convertAndSend(
+                reservaExchange.getName(),
+                "",
+                reservaProducer
+        );
+    }
+}
