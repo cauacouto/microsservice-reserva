630ce7685215e144f7c64453055d7c965ac75abf
cauacouto <cauacouto022@gmail.com>
2026-06-22 23:56:49 -0300

    add: extend ProducerMapper to include email in event mapping

--- src/main/java/dev/couto/microsservice_reserva/Mapping/ProducerMapper.java
+++ src/main/java/dev/couto/microsservice_reserva/Mapping/ProducerMapper.java
@@ -4,16 +4,30 @@ package dev.couto.microsservice_reserva.Mapping;
 import dev.couto.microsservice_reserva.Dto.ReservaProducerDto;
 import dev.couto.microsservice_reserva.domin.Reserva;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProducerMapper {
 
 
-    ReservaProducerDto toEvent(Reserva reserva);
+    @Mapping(source = "reserva.id", target = "reservaId")
+//    @Mapping(source = "reserva.usuarioId", target = "usuarioId")
+//    @Mapping(source = "reserva.salaId", target = "salaId")
+//    @Mapping(source = "reserva.dataInicio", target = "dataHoraInico")
+//    @Mapping(source = "reserva.dataFim", target = "dataHoraFim")
+    @Mapping(source = "email", target = "email")
+    ReservaProducerDto toEvent(Reserva reserva, String email);
+
+
+
+
+
+    }
 
 
 
 
 
 
-}
+
+
